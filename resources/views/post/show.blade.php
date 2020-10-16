0066a3255ba184099701a08813edcbea22dfb992
Dai <let__it__be@hb.tp1.jp>
2020-10-17 08:54:04 +0900

    2020/10/17 8:53

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,44 +3,46 @@
 
 @section('content')
 <div class="posts-page">
+    <label>エディット</label>
     @if($item !== '')
-        {{ $item->title }}: {{ $item->message }}
-        @if(!empty($item->image))
-            <img src="/storage/post/{{ $item->image }}">
-        @endif
+        <div class="list-item">
+            <form action="/post/{{$item->id}}" method="POST" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <input type="hidden" name="user_id" value="{{ $authUser->id }}" />
+                <div>
+                    <input
+                        type="text"
+                        name="title" 
+                        value="{{ $item->title }}"
+                        placeholder="タイトルを入力..."
+                    />
+                </div>
 
-        <form action="/post/{{$item->id}}" method="POST" enctype="multipart/form-data">
-            {{ csrf_field() }}
-            <input type="hidden" name="user_id" value="{{ $authUser->id }}" />
-            <div>
-                <label>title</label>
-                <input
-                    type="text"
-                    name="title" 
-                    value="{{ $item->title }}"
-                    placeholder="title"
-                />
-            </div>
-
-            <div>
-                <label>message</label>
-                <textarea
-                    name="message"
-                    placeholder="message"
-                >{{ $item->message }}</textarea>
-            </div>
-
-            <div>
-                <input type="file" name="thumbnail">
-            </div>
-            
-            <div>
-                <input type="hidden" name="_method" value="PUT" />
-                <input type="submit" value="update" />
-            </div>
-        </form>
+                <div>
+                    <textarea
+                        name="message"
+                        placeholder="メッセージを入力..."
+                    >{{ $item->message }}</textarea>
+                </div>
+                
+                <div class="button">
+                    <label>
+                        <span class="filelabel" title="ファイルを選択">
+                            <img src="/img/upload.png" width="20" height="20" alt="upload">
+                        </span>
+                        <input type="file" class="default-file" name="thumbnail">
+                    </label>
+                    <div>
+                        <input type="hidden" name="_method" value="PUT" />
+                        <input type="submit" class="post-button" value="編集する" />
+                        <a href="/post">戻る</a>
+                    </div>
+                </div>
+                @if(!empty($item->image))
+                    <img src="/storage/post/{{ $item->image }}" style="width:100%;">
+                @endif
+            </form>
+        </div>
     @endif
-
-    <a href="/post">Back</a>
 </div>
 @endsection
